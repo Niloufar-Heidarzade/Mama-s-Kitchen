07e5c7145badc0271f8ffa21aece0fd4c9adc59a
Niloufar Heidarzade <Niiloufar.Heidarzadeh@gmail.com>
2026-07-12 13:54:48 +0330

    created the routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Recipes from "./pages/Recipes";
+import NotFound from "./pages/NotFound";
+import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-    <div>App</div>
-  )
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/recipes" element={<Recipes />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
 }
-
-export default App
